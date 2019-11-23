6b9fd53821354621ee409b532c820144b6d93e21
0xDrazilId <fga-as@mail.ru>
2019-11-23 13:34:17 +0400

    add price list

--- libs/common.js
+++ libs/common.js
@@ -1,5 +1,14 @@
-jQuery(function($){
-    $("#phone").mask("+7 (999) 999-9999");
-    $('.nav__insideList').children("a").on('click', function() {
-        alert(a);
- })};
+ jQuery(function($){
+     $("#phone").mask("+7 (999) 999-9999");
+
+     $('.nav__insideList').children("a").on('click', function() {
+        $('#nav__toggle').prop('checked',false);
+    })
+        
+    $(".tab_item").not(":first").hide();
+    $(".wrapper .tab").click(function() {
+    $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab_item").hide().eq($(this).index()).fadeIn(800)
+}).eq(0).addClass("active");
+
+});
